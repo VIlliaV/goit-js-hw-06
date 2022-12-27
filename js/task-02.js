1a56381f2bddf9f21f34c2485e80d559ac16a931
Illia <illia.vasiliev@gmail.com>
2022-12-27 21:43:55 +0200

    done task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listOfIngridients = document.getElementById(`ingredients`);
+
+const arrLi = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  return liEl;
+});
+
+listOfIngridients.append(...arrLi);
