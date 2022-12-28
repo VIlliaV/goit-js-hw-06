bdf7e1bc8a2468fcc76ead4763d92ad3c92e0161
Illia <illia.vasiliev@gmail.com>
2022-12-28 21:23:01 +0200

    done task 7,8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onTakeInfo);
+
+function onTakeInfo(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "")
+    return alert("не всі поля заповнені");
+  const info = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(info);
+  event.currentTarget.reset();
+}
