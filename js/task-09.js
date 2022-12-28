8f516fea5b65051e1bad2c174b46c7fd79768664
Illia <illia.vasiliev@gmail.com>
2022-12-28 21:59:49 +0200

    done task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector("body");
+const buttonEl = bodyEl.querySelector(".change-color");
+const spanEL = bodyEl.querySelector(".color");
+
+buttonEl.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  spanEL.textContent = `${bodyEl.style.backgroundColor}`;
+}
